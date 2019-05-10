b027d8ab96e19bbbf7b7c53059482e30c2a1ba48
赖章洲 <972389526@qq.com>
2019-05-10 10:40:27 +0800

    代码行修改

--- src/main/java/org/lzz/chat/payconfig/weichat/WeChatPayConfig.java
+++ src/main/java/org/lzz/chat/payconfig/weichat/WeChatPayConfig.java
@@ -4,10 +4,10 @@ public class WeChatPayConfig {
     public WeChatPayConfig() {
     }
 
-    public static final String APPID = "wx426b3015555a46be";    //大陆 wx3e5e848e7220c53a  香港 wx0a2c3b1ad1db4aa4
-    public static final String APPSECRET = "01c6d59a3f9024db6336662ac95c8e74"; // appsecret 1a2a3a
-    public static final String MCH_ID = "1225312702"; // 商业号    大陆 1228571502  香港 1497202262
-    public static final String KEY = "e10adc3949ba59abbe56e057f20f883e"; // 加密key
+    public static final String APPID = "wx426b3015555a46be";
+    public static final String APPSECRET = "01c6d59a3f9024db6336662ac95c8e74";
+    public static final String MCH_ID = "1225312702";
+    public static final String KEY = "e10adc3949ba59abbe56e057f20f883e";
     public static final String TRADE_TYPE_NATIVE = "NATIVE";//扫码支付
     public static final String TRADE_TYPE_APP = "APP";//APP支付
     public static final String TRADE_TYPE_JSAPI = "JSAPI";//公众号支付
